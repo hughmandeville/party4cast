e0632774ec0fbfb3324925beef42385ad577071a
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2016-11-22 10:21:00 -0500

    install

---
+++ install.sh
@@ -0,0 +1,13 @@
+
+
+scp -P 2222 www/code.js muchob5@muchobliged.tv:/home/muchob5/public_html/party4cast/
+scp -P 2222 www/index.html muchob5@muchobliged.tv:/home/muchob5/public_html/party4cast/
+scp -P 2222 www/style.css muchob5@muchobliged.tv:/home/muchob5/public_html/party4cast/
+scp -P 2222 www/admin/index.html muchob5@muchobliged.tv:/home/muchob5/public_html/party4cast/admin/
+
+
+#scp -P 2222 vendor.tar.gz muchob5@muchobliged.tv:/home/muchob5/bin/
+#scp -P 2222 src/process_party_feeds.php muchob5@muchobliged.tv:/home/muchob5/bin/src/
+#scp -P 2222 src/settings.ini muchob5@muchobliged.tv:/home/muchob5/bin/src/
+#scp -P 2222 src/google-project-nyt-video-dashboard.json muchob5@muchobliged.tv:/home/muchob5/bin/src/
+
