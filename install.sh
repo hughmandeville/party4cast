00e77307c46ccd79e846833eb03e57deb1dff5f7
Hubert Mandeville <hubert.mandeville@nytimes.com>
2016-11-25 19:27:41 -0500

    added admin pages.

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 
 
-scp -P 2222 www/code.js muchob5@muchobliged.tv:/home/muchob5/public_html/party4cast/
-scp -P 2222 www/index.html muchob5@muchobliged.tv:/home/muchob5/public_html/party4cast/
-scp -P 2222 www/style.css muchob5@muchobliged.tv:/home/muchob5/public_html/party4cast/
+#scp -P 2222 www/code.js muchob5@muchobliged.tv:/home/muchob5/public_html/party4cast/
+#scp -P 2222 www/index.html muchob5@muchobliged.tv:/home/muchob5/public_html/party4cast/
+#scp -P 2222 www/style.css muchob5@muchobliged.tv:/home/muchob5/public_html/party4cast/
+scp -P 2222 www/admin/code.js muchob5@muchobliged.tv:/home/muchob5/public_html/party4cast/admin/
+#scp -P 2222 www/admin/.htaccess muchob5@muchobliged.tv:/home/muchob5/public_html/party4cast/admin/
 scp -P 2222 www/admin/index.html muchob5@muchobliged.tv:/home/muchob5/public_html/party4cast/admin/
+scp -P 2222 www/admin/style.css muchob5@muchobliged.tv:/home/muchob5/public_html/party4cast/admin/
+#scp -P 2222 www/admin/images/*.png muchob5@muchobliged.tv:/home/muchob5/public_html/party4cast/admin/images/
 
 
 #scp -P 2222 vendor.tar.gz muchob5@muchobliged.tv:/home/muchob5/bin/
